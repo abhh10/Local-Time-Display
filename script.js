44ef795bff8c664fc470d6e6c91ae47c70a1385e
abhh10 <130175061+abhh10@users.noreply.github.com>
2025-08-03 14:26:06 +0530

    comments added

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ setInterval(function(){
     let date = new Date();
     timer.innerHTML = date.toLocaleTimeString();
 }, 1000)
+
+// This code updates the content of an HTML element with the ID 'clock' every second to display the current time in a localized format.
+// It uses the setInterval function to repeatedly execute a function that retrieves the current time and formats it using toLocaleTimeString.
+// The timer will show the time in the user's local timezone and format.
+// Make sure to have an element with id 'clock' in your HTML for this to work
